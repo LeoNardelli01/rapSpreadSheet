4a516f0049c27cab830f2388aad8d81f8abf1eaa
Leonel Nardelli <leonardelli@MacBook-Pro-de-Leonel.local>
2019-06-29 00:19:49 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,26 @@
+
+$(function(){
+
+    var btnEmpezar = $("#empezar");
+
+
+
+    btnEmpezar.click(function(e){
+
+        var rapper1 = $("#r1").val();
+        var rapper2 = $("#r2").val();
+
+          if (rapper1 == "") {
+          e.preventDefault();
+          alert("El Rapero 1 necesita un nombre");
+        } else if(rapper2 == ""){
+          e.preventDefault();
+          alert("El Rapero 2 necesita un nombre");
+        } else{
+          localStorage.setItem('rap1', rapper1);
+          localStorage.setItem('rap2', rapper2);
+        }
+    });
+
+
+})
