1048b62c84cbd3130ad2742f8b2589fb193a8515
Leonel Nardelli <leonardelli@MacBook-Pro-de-Leonel.local>
2019-06-30 20:37:03 -0300

    Revisar si funciona menu y resultados

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,29 @@
 $(function(){
   let player1 = localStorage.getItem('rap1');
   let player2 = localStorage.getItem('rap2');
+  let em_totalP1 = localStorage.getItem('em_totalEasyMode1');
+  let em_totalP2 = localStorage.getItem('em_totalEasyMode2');
 
-  $("#nombre1").html(player1);
-  $("#nombre2").html(player2);
+  if (em_totalP1 === null) {
+    $("#nombre1").html(player1);
+  } else {
+      $("#nombre1").html(player1 + "("+ em_totalP1+ ")");
+  }
+  if (em_totalP2 === null) {
+    $("#nombre2").html(player2);
+  } else {
+    $("#nombre2").html(player2 + "("+ em_totalP2+")");
+  }
+
+
 
   if (localStorage.getItem('em_totalEasyMode1') === null) {
     $("#easyMode").css('background-color', 'rgba(145, 145, 145, 0.51)');//gris
 
   }else{
     $("#easyMode").css('background-color', "rgba(17, 213, 78, 0.39)");//verde
-    $("#easyMode").css('box-shadow', '0px 0px 20px green')
+    $("#easyMode").css('box-shadow', '0px 0px 15px green');
+    $("#easyMode").addClass("disabled");
   }
 
 
--- js/resultados.js
+++ js/resultados.js
@@ -1,26 +1,65 @@
 $(function(){
-    let totalP1 = localStorage.getItem('em_totalEasyMode1');
+    //--- Player 1---
     let player1 = localStorage.getItem('rap1');
+    let em_totalPatronesP1 = localStorage.getItem('em_totalPatronesP1');
+    let em_tecnicasp1 = localStorage.getItem('em_tecnicasp1');
+    let em_flowp1 = localStorage.getItem('em_flowp1');
+    let em_escenap1 = localStorage.getItem('em_escenap1');
+    let em_totalP1 = localStorage.getItem('em_totalEasyMode1');
 
-    let totalP2 = localStorage.getItem('em_totalEasyMode2');
+    //--- Player 2 ---
     let player2 = localStorage.getItem('rap2');
-
-    console.log(totalP1);
+    let em_totalPatronesP2 = localStorage.getItem('em_totalPatronesP2');
+    let em_tecnicasp2 = localStorage.getItem('em_tecnicasp2');
+    let em_flowp2 = localStorage.getItem('em_flowp2');
+    let em_escenap2 = localStorage.getItem('em_escenap2')
+    let em_totalP2 = localStorage.getItem('em_totalEasyMode2');
 
     $("#player1").html(player1)
-    $("#res1").html(totalP1);
+    $("#resPatronesP1").html(em_totalPatronesP1);
+    $("#resTecnicasP1").html(em_tecnicasp1);
+    $("#resFlowP1").html(em_flowp1);
+    $("#resEscenaP1").html(em_escenap1)
+    //-----------------
+    $("#totalP1").html(em_totalP1);
+
 
     $("#player2").html(player2);
-    $("#res2").html(totalP2)
+    $("#resPatronesP2").html(em_totalPatronesP2);
+    $("#resTecnicasP2").html(em_tecnicasp2);
+    $("#resFlowP2").html(em_flowp2);
+    $("#resEscenaP2").html(em_escenap2);
+    //--------------
+    $("#totalP2").html(em_totalP2);
 
-    html2canvas(document.body, {
-      onrendered (canvas) {
-        var link = document.getElementById('download');;
-        var image = canvas.toDataURL();
-        link.href = image;
-        link.download = player1 + '(' + totalP1 + ')' + ' vs ' + player2 + '(' + totalP2 + ')' + '.png';
-  }
- });
+
+    var sum_totales = parseInt(em_totalP1) + parseInt(em_totalP2);
+
+    if (parseInt(em_totalP1) > parseInt(em_totalP2)) {
+      $("#ganador").html("GANO " + player1);
+    } else if (parseInt(em_totalP2) > parseInt(em_totalP1)) {
+      $("#ganador").html("GANO " + player2);
+    } else{
+      $("#ganador").html("EMPATE!");
+      var btn_replica = "<a id='replica' class='btn btn-warning btn-lg btn-block' href='replica.html'>IR A REPLICA</a>";
+      $("#replica").append(btn_replica);
+    }
+
+
+
+
+
+
+//    html2canvas(document.body, {
+//      onrendered (canvas) {
+//        var link = document.getElementById('download');;
+//        var image = canvas.toDataURL();
+//        link.href = image;
+//        link.download = player1 + ' vs ' + player2 + '.png';
+//  }
+//});
+
+
     $("#nuevaBatalla").click(function(e){
       var r = confirm("Estas seguro?\n");
 
